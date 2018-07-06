daf9261b6084e73a17d0e0d61652269600a4a251
CubeBreak <lokmane1336@hotmail.de>
2018-07-06 20:40:02 +0200

    Added Example 1.1

---
+++ C++/src/main.cpp
@@ -0,0 +1,30 @@
+﻿/*
+	File: main.cpp
+	Author: Lokman "CubeBreak" Ahmed
+	Created on 6th July 2018
+*/
+
+#include <iostream>
+
+#pragma region Example 1.1
+#if 1
+
+int main() 
+{
+	int summe;
+	int summand1;
+	int summand2;
+	 // Lies zwei Zahlenstd::
+	std::cout << "Zwei ganze Zahlen eingeben:";
+	std::cin >> summand1 >> summand2;
+	/* Berechne die Summe beider Zahlen */
+	summe = summand1 + summand2;	// Zeige das Ergebnis auf dem Bildschirm an
+	std::cout << "Summe = " << summe << std::endl;
+
+	system("pause");
+
+	return 0;
+}
+
+#endif
+#pragma endregion
